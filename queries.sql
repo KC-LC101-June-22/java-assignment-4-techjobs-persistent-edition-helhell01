a013d907495eeb43521346842fd6e2f27d2fe400
Hanna Luechtefeld <hannaluechtefeld@gmail.com>
2022-11-30 21:07:39 -0600

    everything up to Test It W SQL

--- queries.sql
+++ queries.sql
@@ -1,4 +1,6 @@
 -- Part 1: Test it with SQL
+-- Columns: id, employer, name, skills
+-- Row data: null
 
 -- Part 2: Test it with SQL
 
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Size(min=2,max=100, message = "Location must be between 2 and 100 characters long")
+    private String location;
 
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
