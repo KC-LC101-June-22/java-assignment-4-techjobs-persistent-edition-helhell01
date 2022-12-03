2cdc372f2e18f696f6dd392d1509dfda325c472d
Hanna Luechtefeld <hannaluechtefeld@gmail.com>
2022-12-03 12:47:22 -0600

    finished assignment 4

--- queries.sql
+++ queries.sql
@@ -1,9 +1,15 @@
 -- Part 1: Test it with SQL
--- Columns: id, employer, name, skills
--- Row data: null
+-- id: int, employer: varchar, name: varchar, skills: varchar
 
 -- Part 2: Test it with SQL
+SELECT name FROM employer WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
+DROP TABLE job;
 
 -- Part 4: Test it with SQL
+--write a query to return the names of all skills that are attached to jobs in alphabetical order.
+--If a skill does not have a job listed, it should not be included in the results of this query
+SELECT * FROM skill INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,11 +1,21 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
 
     @NotBlank
     @Size(min=2,max=100, message = "Location must be between 2 and 100 characters long")
@@ -13,6 +23,14 @@ public class Employer extends AbstractEntity {
 
     public Employer() {
     }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs() {
+        this.jobs = jobs;
+    };
 
     public String getLocation() {
         return location;
